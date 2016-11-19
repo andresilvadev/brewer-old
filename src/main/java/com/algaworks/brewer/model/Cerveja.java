aee2b40e10be97aafe069e8ebe1432128eb3100e
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-11-19 12:38:47 -0200

    Validacao de formularios

---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,29 @@
+package com.algaworks.brewer.model;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+	
+	@NotBlank
+	private String sku;
+	
+	@NotNull
+	private String nome;
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
