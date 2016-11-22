e22959802ec189c29d62dc3745fed694a5f7858d
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-11-21 23:50:46 -0200

    Prototipando o cadastro de estilo

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UsuariosController {
 
-	@RequestMapping("usuarios/novo")
+	@RequestMapping("/usuarios/novo")
 	public String novo(){
 		return "usuario/CadastroUsuario";
 	}
