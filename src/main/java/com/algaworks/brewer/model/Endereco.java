03962d890ab985adee8d39d1b7c3dfe04c944aea
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-04-14 20:57:31 -0300

    Aplicando migracao de cliente

---
+++ src/main/java/com/algaworks/brewer/model/Endereco.java
@@ -0,0 +1,52 @@
+package com.algaworks.brewer.model;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class Endereco {
+	
+	private String logradouro;
+	private String numero;
+	private String cep;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "codigo_cidade")
+	private Cidade cidade;
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getCep() {
+		return cep;
+	}
+	
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	public Cidade getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+}
