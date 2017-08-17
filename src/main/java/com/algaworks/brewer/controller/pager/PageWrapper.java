06c501c15be7d1b2ca18c4a50d4cff6e9526acc0
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-08-17 12:08:08 -0300

    Pesquisa de clientes

--- src/main/java/com/algaworks/brewer/controller/pager/PageWrapper.java
+++ src/main/java/com/algaworks/brewer/controller/pager/PageWrapper.java
@@ -15,11 +15,10 @@ public class PageWrapper<T> {
 	private UriComponentsBuilder uriBuilder;
 	
 	public PageWrapper(Page<T> page, HttpServletRequest httpServletRequest){
-		this.page = page;
-		//this.uriBuilder = ServletUriComponentsBuilder.fromRequest(httpServletRequest);
+		this.page = page;		
 		String httpUrl = httpServletRequest.getRequestURL().append(
-			httpServletRequest.getQueryString() != null ? "?" + httpServletRequest.getQueryString() : "")
-				.toString().replaceAll("\\+", "%20");
+						 httpServletRequest.getQueryString() != null ? "?" + httpServletRequest.getQueryString() : "")
+						 .toString().replaceAll("\\+", "%20");
 				this.uriBuilder = UriComponentsBuilder.fromHttpUrl(httpUrl);
 	}
 	
