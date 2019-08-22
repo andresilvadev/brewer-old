b1acfa7c221c4d19d20674ac906fd9cbe995889c
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-08-21 21:39:47 -0300

    Implementando pesquisa e cadastro de cidades

--- src/main/resources/db/migration/V04__adicionar_cidade_e_estado.sql
+++ src/main/resources/db/migration/V04__adicionar_cidade_e_estado.sql
@@ -5,7 +5,7 @@ CREATE TABLE estado (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE cidade (
-    codigo BIGINT(20) PRIMARY KEY,
+    codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     codigo_estado BIGINT(20) NOT NULL,
     FOREIGN KEY (codigo_estado) REFERENCES estado(codigo)
