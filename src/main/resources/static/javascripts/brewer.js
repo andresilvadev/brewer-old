625a6935aa93775bdcf8397bf0d8bb395746dbb5
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-09-05 20:29:27 -0300

    19.9. Configurando o CSRF

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -85,6 +85,26 @@ Brewer.MaskDate = (function(){
 	
 }());
 
+/**
+ * Função que adiciona token nas requisições ajax
+ */
+Brewer.Security = (function() {
+	
+	function Security() {
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header]').val();
+	}
+	
+	Security.prototype.enable = function() {
+		$(document).ajaxSend(function(event, jqxhr, settings) {
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+	
+}());
+
 
 $(function(){
 	
@@ -99,5 +119,8 @@ $(function(){
 	
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+	
+	var security = new Brewer.Security();
+	security.enable();
 		
 });
