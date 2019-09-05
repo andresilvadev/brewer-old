792254a092e2b7f01c12ee538e31e14273d6c916
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-09-04 21:16:07 -0300

    19.4. Autenticando usuário e senha no banco de dados

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -1,23 +1,35 @@
 package com.algaworks.brewer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.algaworks.brewer.security.AppUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackageClasses = AppUserDetailsService.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+		// Login de teste
+		//auth.inMemoryAuthentication()
+		//	.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+		
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
@@ -31,8 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()			
-			.anyRequest().authenticated()
-			.and()
+				.anyRequest().authenticated()
+				.and()
 			.formLogin()
 				.loginPage("/login")
 				.permitAll()
