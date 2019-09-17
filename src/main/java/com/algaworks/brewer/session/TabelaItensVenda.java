d3738f4f9e13336e0142f49c314c7f46203c6001
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-09-17 14:14:28 -0300

    23.7. Adicionando mesmos itens na venda

--- src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
+++ src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.session;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
@@ -25,12 +26,24 @@ public class TabelaItensVenda {
 	
 	
 	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
-		ItemVenda itemVenda = new ItemVenda();
-		itemVenda.setCerveja(cerveja);
-		itemVenda.setQuantidade(quantidade);
-		itemVenda.setValorUnitario(cerveja.getValor());
+		Optional<ItemVenda> itemVendaOptional = itens.stream()
+			.filter(i -> i.getCerveja().equals(cerveja))
+			.findAny();
 		
-		itens.add(itemVenda);
+		ItemVenda itemVenda = null;
+		
+		if(itemVendaOptional.isPresent()) {
+			itemVenda = itemVendaOptional.get();
+			itemVenda.setQuantidade(itemVenda.getQuantidade() + quantidade);
+		} else {
+			itemVenda = new ItemVenda();
+			itemVenda.setCerveja(cerveja);
+			itemVenda.setQuantidade(quantidade);
+			itemVenda.setValorUnitario(cerveja.getValor());
+			
+			itens.add(0, itemVenda);
+		}
+		
 	}
 
 
@@ -38,7 +51,7 @@ public class TabelaItensVenda {
 		return itens.size();
 	}
 	
-	public Object getItens() {
+	public List<ItemVenda> getItens() {
 		return itens;
 	}
 	
