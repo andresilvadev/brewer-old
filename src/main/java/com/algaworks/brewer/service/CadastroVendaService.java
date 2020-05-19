16eb49d5d8a4fe91aba016e1016716cae2296909
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-05-19 15:11:45 -0300

    25.5. Implementando o cancelamento da venda

--- src/main/java/com/algaworks/brewer/service/CadastroVendaService.java
+++ src/main/java/com/algaworks/brewer/service/CadastroVendaService.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,10 @@ public class CadastroVendaService {
 	
 	@Transactional
 	public Venda salvar(Venda venda) {
+		if(venda.isSalvarProibido()) {
+			throw new RuntimeException("Usuário tentando salvar uma venda proibida");
+		}
+		
 		if(venda.isNova()) {
 			venda.setDataCriacao(LocalDateTime.now());
 		} else {
@@ -41,6 +46,15 @@ public class CadastroVendaService {
 	public void emitir(Venda venda) {
 		venda.setStatus(StatusVenda.EMITIDA);
 		salvar(venda);
+	}
+	
+	@PreAuthorize("#venda.usuario == principal.usuario or hasRole('CANCELAR_VENDA')")
+	@Transactional
+	public void cancelar(Venda venda) {
+		Venda vendaExistente = vendas.findOne(venda.getCodigo());
+		
+		vendaExistente.setStatus(StatusVenda.CANCELADA);
+		vendas.save(vendaExistente);
 	}
 
 }
